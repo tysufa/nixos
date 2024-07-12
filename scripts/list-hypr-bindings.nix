0173bce466ce687d8eda36730cc02c91f6316b73
tysufa <philemon.penot@gmail.com>
2024-07-12 14:48:18 +0200

    deleted and modified many keybindings to stick to the ones I know best

--- scripts/list-hypr-bindings.nix
+++ scripts/list-hypr-bindings.nix
@@ -17,40 +17,27 @@ pkgs.writeShellScriptBin "list-hypr-bindings" ''
   --timeout-indicator=right \
   " = Windows/Super/CAPS LOCK" "Modifier Key, used for keybindings" "Doesn't really execute anything by itself." \
   " + ENTER" "Terminal" "${terminal}" \
+  " + D" "Terminal" "${terminal}" \
   " + SHIFT + ENTER" "App Launcher" "rofi" \
   " + ALT + W" "Change Wallpaper" "wallsetter" \
   " + Q" "Kill Focused Window" "killactive" \
-  " + SHIFT + W" "Search Websites Like Nix Packages" "web-search" \
+  " + ALT + F" "Search Websites Like Nix Packages" "web-search" \
   " + SHIFT + N" "Reload SwayNC Styling" "swaync-client -rs" \
-  " + W" "Launch Web Browser" "${browser}" \
-  " + E" "Launch Emoji Selector" "emopicker9000" \
-  " + S" "Take Screenshot" "screenshootin" \
+  " + F" "Launch Web Browser" "${browser}" \
+  " + SHIFT + E" "Launch Emoji Selector" "emopicker9000" \
+  " + SHIFT + S" "Take Screenshot" "screenshootin" \
   " + D" "Launch Discord" "discord" \
-  " + O" "Launch OBS" "obs" \
-  " + G" "Launch GIMP" "gimp" \
-  " + N" "Launch New File Browser Window" "thunar" \
-  " + M" "Launch Spotify" "spotify" \
+  " + E" "Launch New File Browser Window" "thunar" \
+  " + S" "Launch Spotify" "spotify" \
   " + P" "Pseudo Tiling" "pseudo" \
   " + SHIFT + I" "Toggle Split Direction" "togglesplit" \
-  " + F" "Toggle Focused Fullscreen" "fullscreen" \
-  " + SHIFT + F" "Toggle Focused Floating" "fullscreen" \
+  " + SHIFT + F" "Toggle Focused Fullscreen" "fullscreen" \
+  " + V" "Toggle Focused Floating" "fullscreen" \
   " + SHIFT + C" "Quit / Exit Hyprland" "exit" \
-  " + Left" "Move Focus To Window On The Left" "movefocus,l" \
-  " + Right" "Move Focus To Window On The Right" "movefocus,r" \
-  " + Up" "Move Focus To Window On The Up" "movefocus,u" \
-  " + Down" "Move Focus To Window On The Down" "movefocus,d" \
-  " + SHIFT + Left" "Move Focused Window Left" "movewindow,l" \
-  " + SHIFT + Right" "Move Focused Window Right" "movewindow,r" \
-  " + SHIFT + Up" "Move Focused Window Up" "movewindow,u" \
-  " + SHIFT + Down" "Move Focused Window Down" "movewindow,d" \
-  " + H" "Move Focus To Window On The Left" "movefocus,l" \
-  " + L" "Move Focus To Window On The Right" "movefocus,r" \
-  " + K" "Move Focus To Window On The Up" "movefocus,u" \
-  " + J" "Move Focus To Window On The Down" "movefocus,d" \
-  " + SHIFT + H" "Move Focused Window Left" "movewindow,l" \
-  " + SHIFT + L" "Move Focused Window Right" "movewindow,r" \
-  " + SHIFT + K" "Move Focused Window Up" "movewindow,u" \
-  " + SHIFT + J" "Move Focused Window Down" "movewindow,d" \
+  " + Left/Right/Up/Down" "Move Focus To Window On The Left/Right/Up/Down" "movefocus,l/r/u/d" \
+  " + SHIFT + Left/Right/Up/Down" "Move Focused Window Left/Right/Up/Down" "movewindow,l/r/u/d" \
+  " + H/L/K/J" "Move Focus To Window On The Left/Right/Up/Down" "movefocus,l/r/u/d" \
+  " + SHIFT + H/L/K/J" "Move Focused Window Left/Right/Up/Down" "movewindow,l/r/u/d" \
   " + SPACE" "Toggle Special Workspace" "togglespecialworkspace" \
   " + SHIFT + SPACE" "Send Focused Window To Special Workspace" "movetoworkspace,special" \
   " + 1-0" "Move To Workspace 1 - 10" "workspace,X" \
