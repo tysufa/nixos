aeb1af536811b46c506252b3c165425faa0a978b
tysufa <philemon.penot@gmail.com>
2024-07-12 14:48:53 +0200

    deleted a few useless programs

--- hosts/nixOs/config.nix
+++ hosts/nixOs/config.nix
@@ -55,24 +55,24 @@
     enable = true;
     image = backgroundImage;
     # catpuccin colorscheme
-    base16Scheme = {
-        base00 = "1e1e2e"; # base
-        base01 = "181825"; # mantle
-        base02 = "313244"; # surface0
-        base03 = "45475a"; # surface1
-        base04 = "585b70"; # surface2
-        base05 = "cdd6f4"; # text
-        base06 = "f5e0dc"; # rosewater
-        base07 = "b4befe"; # lavender
-        base08 = "f38ba8"; # red
-        base09 = "fab387"; # peach
-        base0A = "f9e2af"; # yellow
-        base0B = "a6e3a1"; # green
-        base0C = "94e2d5"; # teal
-        base0D = "89b4fa"; # blue
-        base0E = "cba6f7"; # mauve
-        base0F = "f2cdcd"; # flamingo    
-    };
+    # base16Scheme = {
+    #     base00 = "1e1e2e"; # base
+    #     base01 = "181825"; # mantle
+    #     base02 = "313244"; # surface0
+    #     base03 = "45475a"; # surface1
+    #     base04 = "585b70"; # surface2
+    #     base05 = "cdd6f4"; # text
+    #     base06 = "f5e0dc"; # rosewater
+    #     base07 = "b4befe"; # lavender
+    #     base08 = "f38ba8"; # red
+    #     base09 = "fab387"; # peach
+    #     base0A = "f9e2af"; # yellow
+    #     base0B = "a6e3a1"; # green
+    #     base0C = "94e2d5"; # teal
+    #     base0D = "89b4fa"; # blue
+    #     base0E = "cba6f7"; # mauve
+    #     base0F = "f2cdcd"; # flamingo    
+    # };
     polarity = "dark";
     opacity.terminal = 0.8;
     cursor.package = pkgs.bibata-cursors;
@@ -137,75 +137,6 @@
 
   programs = {
     firefox.enable = true;
-    starship = {
-      enable = false;
-      settings = {
-        add_newline = false;
-        buf = {
-          symbol = " ";
-        };
-        c = {
-          symbol = " ";
-        };
-        directory = {
-          read_only = " 󰌾";
-        };
-        docker_context = {
-          symbol = " ";
-        };
-        fossil_branch = {
-          symbol = " ";
-        };
-        git_branch = {
-          symbol = " ";
-        };
-        golang = {
-          symbol = " ";
-        };
-        hg_branch = {
-          symbol = " ";
-        };
-        hostname = {
-          ssh_symbol = " ";
-        };
-        lua = {
-          symbol = " ";
-        };
-        memory_usage = {
-          symbol = "󰍛 ";
-        };
-        meson = {
-          symbol = "󰔷 ";
-        };
-        nim = {
-          symbol = "󰆥 ";
-        };
-        nix_shell = {
-          symbol = " ";
-        };
-        nodejs = {
-          symbol = " ";
-        };
-        ocaml = {
-          symbol = " ";
-        };
-        package = {
-          symbol = "󰏗 ";
-        };
-        python = {
-          symbol = " ";
-        };
-        rust = {
-          symbol = " ";
-        };
-        swift = {
-          symbol = " ";
-        };
-        zig = {
-          symbol = " ";
-        };
-      };
-    };
     dconf.enable = true;
     seahorse.enable = true;
     fuse.userAllowOther = true;
@@ -246,15 +177,12 @@
     eza
     git
     cmatrix
-    lolcat
     fastfetch
-    htop
     brave
     libvirt
     lxqt.lxqt-policykit
     lm_sensors
     unzip
-    unrar
     libnotify
     v4l-utils
     ydotool
@@ -264,14 +192,12 @@
     pciutils
     ffmpeg
     socat
-    cowsay
     ripgrep
     lshw
     bat
     pkg-config
     meson
     hyprpicker
-    ninja
     brightnessctl
     virt-viewer
     swappy
@@ -291,7 +217,6 @@
     swaynotificationcenter
     imv
     mpv
-    gimp
     pavucontrol
     tree
     spotify
@@ -335,7 +260,7 @@
     xserver = {
       enable = false;
       xkb = {
-        layout = "us";
+        layout = "fr";
         variant = "";
       };
     };
