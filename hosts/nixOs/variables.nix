c05e526aa3f7aa13edec949a9b8ab8572d558835
tysufa <philemon.penot@gmail.com>
2024-09-03 16:01:26 +0200

    changed default browser to zen browser (based on firefox)

--- hosts/nixOs/variables.nix
+++ hosts/nixOs/variables.nix
@@ -10,6 +10,6 @@
   clock24h = true;
 
   # Program Options
-  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  browser = "zen"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
 }
